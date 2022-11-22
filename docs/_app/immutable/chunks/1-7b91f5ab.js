c4d296d6aa454d9464b112207ad5bf266ab794bc
Matt <mw4854@nyu.edu>
2022-11-21 22:38:02 -0500

    updated about

--- docs/_app/immutable/chunks/1-7b91f5ab.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-f11827d0.js";export{t as component};
--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,8 +1,11 @@
 <h2>About</h2>
-<p>Hi, I'm Matt - thanks for visiting my website!</p>
+<p>
+	I'm a game designer and programmer currently pursuing an MFA at the NYU Game Center. I also have a
+	background in music composition and audio programming and dabble in 2D art and animation.
+</p>
 <p>
 	I'm interested in generative and procedural systems, emergent play, experimental games, music
-	games, and more.
+	games, web games, and more.
 </p>
 <p>
 	Currently, I make lots of small games and prototypes, which are playable on my
@@ -10,5 +13,6 @@
 </p>
 <p>
 	You can contact me via email at <a href="mailto:mattwmora@gmail.com">mattwmora@gmail.com</a> or on
-	Discord as mattmora#7958.
+	Discord as mattmora#7958, and you can see my resume
+	<a href="/Matt_Wang_Resume.pdf" target="_blank" rel="noopener noreferrer">here</a>.
 </p>
