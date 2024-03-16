34064e36338a3b05fbd481d194de793c21d5be09
Matt <mw4854@nyu.edu>
2024-03-16 15:20:31 -0400

    update docs

--- docs/_app/immutable/chunks/1-9dc26449.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-7b37e672.js";export{t as component};
--- docs/_app/immutable/chunks/2-a962617a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-2182cd0f.js";export{t as component};
